b073f7a78d6465ebb36129f15b1e193466f1ed1e
Tommaso Clini <tommasoclini@gmail.com>
2023-06-18 12:06:25 +0200

    Remove ignored files

--- gestione_schede_lora_1/regex_formats.py
+++
@@ -1,7 +0,0 @@
-general_message_format = rb"^(?P<message_type>[PUS])(?P<message_content>[^\n]+)\n.$"
-
-pub_request_format = rb"^(?P<topic_name>[^\r]+)\r(?P<message_content>.+)$"
-
-sub_request_format = rb"^(?P<board_informations>.{3})(?P<topic_name>.+)$"
-
-unsub_request_format = sub_request_format
